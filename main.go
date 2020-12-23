9a2705a1ca0c94606a6566c2d4911480a9f1530d
Kasim Dilsiz <ksmdlsz@gmail.com>
2020-12-23 15:13:16 +0300

    Go Rotinleri ile ilgili çalışmalar yapıldı.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type circle struct {
+	radius int
+}
+
+func (a *circle) area() int {
+	return a.radius * a.radius * 3
+
+}
+func (a *circle) perimeter() int {
+	return 2 * 3 * a.radius
+
+}
+
+func main() {
+	b := circle{radius: 4}
+	fmt.Println(b.perimeter(), "cevre")
+	fmt.Println(b.area(), "alan")
+
+}
